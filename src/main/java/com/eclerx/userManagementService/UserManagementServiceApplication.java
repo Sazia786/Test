d663d3921dad4c17f5c2dd12545c98b965ce9a0d
Sazia.Siddiqui <Sazia.Siddiqui@MUDT2ZDP9W2.india.eclerx.com>
2020-01-28 16:05:01 +0530

    initial commit and update

--- src/main/java/com/eclerx/userManagementService/UserManagementServiceApplication.java
+++ src/main/java/com/eclerx/userManagementService/UserManagementServiceApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserManagementServiceApplication {
 
 	public static void main(String[] args) {
+		
+		//test comment
+		
 		SpringApplication.run(UserManagementServiceApplication.class, args);
 	}
 
